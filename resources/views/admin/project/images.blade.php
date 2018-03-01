d38c80ca5be198f7e5d38069d375930ac2661f14
Kevin M <kevin@dcode.com.au>
2018-03-01 21:39:30 +1100

    image upload appears in front end

--- resources/views/admin/project/images.blade.php
+++ resources/views/admin/project/images.blade.php
@@ -6,13 +6,15 @@
             <h1>{{$record->title}}'s images</h1>
             <div class="grid-x grid-margin-x raised">
                 <div class="cell small-6">
-                    <div class="image-dropzone" data-id="lead_image">
-
+                    <div class="image-dropzone" data-id="{{$record->id}}" data-name="lead_image">
+                        <div class="dz-message"><span class="lnr lnr-cloud-upload"></span></div>
+                        <div class="dz-message">Upload Lead Image</div>
                     </div>
                 </div>
                 <div class="cell small-6">
-                    <div class="image-dropzone" data-id="background_image">
-
+                    <div class="image-dropzone" data-id="{{$record->id}}" data-name="background_image">
+                        <div class="dz-message"><span class="lnr lnr-cloud-upload"></span></div>
+                        <div class="dz-message">Upload Background Image</div>
                     </div>
                 </div>
             </div>
@@ -20,3 +22,23 @@
         <div class="cell small-2"></div>
     </div>
 @endsection
+@push('javascript')
+<script>
+    $(document).ready(function(){
+        var token = $("meta[name='csrf-token']").attr('content');
+        $("div.image-dropzone").each(function(){
+            var id = $(this).data('id');
+            var name = $(this).data('name');
+            console.log('dropzoning: ' +id + ' ' +name)
+            $(this).dropzone({
+                url : '/admin/image/upload',
+                paramName : name,
+                maxFiles: 1,
+                headers : {
+                    'X-CSRF-TOKEN' : token
+                }
+            });
+        });
+    });
+</script>
+@endpush

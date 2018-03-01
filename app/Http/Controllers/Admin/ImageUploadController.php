3254c6756bcfcc8a275e8f87aa28a2a3dcc0c603
Kevin M <kevin@dcode.com.au>
2018-03-01 22:42:29 +1100

    can upload images

--- app/Http/Controllers/Admin/ImageUploadController.php
+++ app/Http/Controllers/Admin/ImageUploadController.php
@@ -3,12 +3,34 @@
 namespace Vhom\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Vhom\Http\Controllers\Controller;
+use Vhom\Project;
 
 class ImageUploadController extends Controller
 {
     public function upload(Request $request)
     {
-
+        if($request->hasFile('background_image')){
+            if(isset($request->id)){
+                $file = $request->file('background_image');
+                $ext  = $file->getClientOriginalExtension();
+                $name = 'projects/background/'.bin2hex(random_bytes(8)).".$ext";
+                Storage::disk('local')->put($name,file_get_contents($file),'public');
+                Project::find($request->id)->update([
+                    'background_image' => $name
+                ]);
+            }
+        } else if ($request->hasFile('lead_image')){
+            if(isset($request->id)){
+                $file = $request->file('lead_image');
+                $ext  = $file->getClientOriginalExtension();
+                $name = 'projects/lead/'.bin2hex(random_bytes(8)).".$ext";
+                Storage::disk('local')->put($name,file_get_contents($file),'public');
+                Project::find($request->id)->update([
+                    'lead_image' => $name
+                ]);
+            }
+        }
     }
 }
--- resources/views/admin/project/images.blade.php
+++ resources/views/admin/project/images.blade.php
@@ -4,17 +4,42 @@
         <div class="cell small-2"></div>
         <div class="cell small-8 raised">
             <h1>{{$record->title}}'s images</h1>
+            <p>Uploading more will overwrite.</p>
             <div class="grid-x grid-margin-x raised">
                 <div class="cell small-6">
                     <div class="image-dropzone" data-id="{{$record->id}}" data-name="lead_image">
                         <div class="dz-message"><span class="lnr lnr-cloud-upload"></span></div>
                         <div class="dz-message">Upload Lead Image</div>
+                        @if(isset($record->lead_image))
+                            <div class="dz-preview">
+                                <div class="dz-image">
+                                    <img src="{{asset('storage/app/'.$record->lead_image)}}" alt="">
+                                </div>
+                                <div class="dz-details">
+                                    <div class="dz-filename">
+                                        Current Image
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
                     </div>
                 </div>
                 <div class="cell small-6">
                     <div class="image-dropzone" data-id="{{$record->id}}" data-name="background_image">
                         <div class="dz-message"><span class="lnr lnr-cloud-upload"></span></div>
                         <div class="dz-message">Upload Background Image</div>
+                        @if(isset($record->background_image))
+                            <div class="dz-preview">
+                                <div class="dz-image">
+                                    <img src="{{asset('storage/app/'.$record->background_image)}}" alt="">
+                                </div>
+                                <div class="dz-details">
+                                    <div class="dz-filename">
+                                        Current Image
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
@@ -36,6 +61,9 @@
                 maxFiles: 1,
                 headers : {
                     'X-CSRF-TOKEN' : token
+                },
+                sending: function(file, xhr, formData){
+                    formData.append('id', id);
                 }
             });
         });

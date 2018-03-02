a1ece9f25e610905149dfa89793dd62c0e059b75
Kevin M <kevin@dcode.com.au>
2018-03-02 19:26:06 +1100

    add form checks on project/edit

--- resources/views/admin/project/edit.blade.php
+++ resources/views/admin/project/edit.blade.php
@@ -4,24 +4,24 @@
     <div class="grid-x grid-padding-x">
         <div class="cell small-12">
             <label for="title">Title:
-                <input type="text" name="title" value="{{$record ? $record->title :"" }}">
+                <input type="text" name="title" value="{{ isset($record) ? $record->title :"" }}">
             </label>
         </div>
         <div class="cell small-12">
             <label for="subtitle">Subtitle:
-                <input type="text" name="subtitle" value="{{$record ? $record->subtitle : ""}}">
+                <input type="text" name="subtitle" value="{{isset($record) ? $record->subtitle : ""}}">
             </label>
         </div>
         <div class="cell small-12">
             <label for="body">Body:
                 <textarea name="body" class="rte" rows="10">
-                    {{ $record ? htmlspecialchars($record->body) : "" }}
+                    {{ isset($record) ? htmlspecialchars($record->body) : "" }}
                 </textarea>
             </label>
         </div>
         <div class="cell small-12">
             <label for="github">Github link:
-                <input type="text" name="github" value="{{$record ? $record->github : ""}}">
+                <input type="text" name="github" value="{{isset($record) ? $record->github : ""}}">
             </label>
         </div>
     </div>
